6de033bcb8081fc64aadd6f40b74a67dbc4302be
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2025-01-10 23:56:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,8 +9,6 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-rm -rf feeds/luci/applications/luci-app-openclash
-
 
 # 添加 onliner 插件
 git clone https://github.com/nhhqgirl/luci-app-onliner.git package/lean/luci-app-onliner
