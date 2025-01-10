211f7d0a4cea6a5b08b90805546a84695fcbe952
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2025-01-10 23:30:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,59 +9,17 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
+rm -rf feeds/luci/applications/luci-app-openclash
+git clone https://github.com/nhyoungboy/luci-app-openclash.git package/lean/luci-app-openclash
 
-#sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
+# 添加 onliner 插件
+git clone https://github.com/nhhqgirl/luci-app-onliner.git package/lean/luci-app-onliner
 
-# 添加ssrp
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
-#rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
-rm -rf package/helloworld
-#git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
-git clone -b master --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-#git clone -b main --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
-#sed -i '/define Package\/mosdns\/install/i GO_PKG_TARGET_VARS:=$(filter-out CGO_ENABLED=%,$(GO_PKG_TARGET_VARS)) CGO_ENABLED=1\n' package/helloworld/mosdns/Makefile
+# 添加 PowerOff 插件
+# git clone https://github.com/nhhqgirl/luci-app-poweroff.git package/lean/luci-app-poweroff
 
-# 替换argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+# 添加 opentomcat 主题
+# git clone --depth=1 https://github.com/nhyoungboy/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
 
-# 替换serverchan
-git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush.git package/luci-app-serverchan
-
-# 添加oaf
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-
-# 添加ddns-go
-# git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
-
-# 添加lucky
-git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
-
-# 添加watchcat-plus
-#git clone https://github.com/openwrt/packages
-#mv packages/utils/watchcat package/utils
-#rm -rf packages
-#git clone https://github.com/gngpp/luci-app-watchcat-plus.git package/luci-app-watchcat-plus
-git clone https://github.com/sirpdboy/sirpdboy-package
-mv sirpdboy-package/watchcat package/utils
-mv sirpdboy-package/luci-app-watchcat-plus package
-rm -rf sirpdboy-package
-
-# 添加ap-modem
-# svn export https://github.com/linkease/openwrt-app-actions/trunk/applications/luci-app-ap-modem package/luci-app-ap-modem
-
-# iStore
-# svn export https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-store-ui
-# svn export https://github.com/linkease/istore/trunk/luci package/luci-app-store
-
-# 在线用户
-# svn export https://github.com/haiibo/packages/trunk/luci-app-onliner package/luci-app-onliner
-git clone https://github.com/haiibo/packages
-mv packages/luci-app-onliner package
-rm -rf packages
-sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=2s' package/lean/default-settings/files/zzz-default-settings
-sed -i '$i uci commit nlbwmon' package/lean/default-settings/files/zzz-default-settings
-chmod 755 package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
+# 添加 argon 主题
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
