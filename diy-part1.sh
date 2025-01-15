a5e19e98d020b73ba532166679e53b1337ac7e54
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2025-01-15 14:12:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,21 +10,13 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# 添加 Openclash 插件
-# wget -O package/openclash.zip https://codeload.github.com/vernesong/OpenClash/zip/refs/heads/master
-# unzip -d package/openclash package/openclash.zip
-# cp -r package/openclash/OpenClash-master/luci-app-openclash package/lean/luci-app-openclash
-# rm -rf package/openclash
-# rm -f package/openclash.zip
-
 # 添加 onliner 插件
 git clone https://github.com/nhhqgirl/luci-app-onliner.git package/lean/luci-app-onliner
 
 # 添加 PowerOff 插件
-# git clone https://github.com/nhhqgirl/luci-app-poweroff.git package/lean/luci-app-poweroff
+git clone https://github.com/nhhqgirl/luci-app-poweroff.git package/lean/luci-app-poweroff
 
 # 添加 opentomcat 主题
-# git clone --depth=1 https://github.com/nhyoungboy/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
+git clone --depth=1 https://github.com/nhyoungboy/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
 
-# 添加 argon 主题
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+
